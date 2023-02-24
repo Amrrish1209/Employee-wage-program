9a15f4a962a63e22ac0dd12839eefa028def31dc
Amrrish1209 <roshanbabubl@gmail.com>
2023-02-24 19:49:27 +0530

    [Amrrish] Add : Shell script program to calculate daily employee wage

--- emp.sh
+++ emp.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo "Checking Employee is present or absent"
+echo "Checking Employee is present or absent if present then we want to calculate the Daily wage"
 
 res=$(( RANDOM % 2 ))
 
 if [ $res -eq 1 ]
 then
-	echo "Employee is present"
+	WagePerHour=20
+	FullDayHour=8
+	EmpWageSalary=$(( WagePerHour * FullDayHour ))
+	echo "Daily Employee Wage is:" $EmpWageSalary
 else
 	echo "Employee is absent"
 fi
