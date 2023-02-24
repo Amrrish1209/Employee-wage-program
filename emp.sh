19825906e9582cde99c5e94af300025531d78841
Amrrish1209 <roshanbabubl@gmail.com>
2023-02-24 20:12:40 +0530

    [Amrrish] Add : Shell script program to displaying employee wage solving using switch case

--- emp.sh
+++ emp.sh
@@ -4,19 +4,22 @@ echo "Checking Employee is present or absent if present then we want to calculat
 
 res=$(( RANDOM % 2 ))
 
-if [ $res -eq 1 ]
-then
-	echo "Enter the Employee working hour as 4hour or 8hours"
+case $res in 
+1)
+	echo "Checking the employee working as fullday or parttime and giving salary"
 	fulldayhour=$(( RANDOM % 2 == 0 ? 4 : 8 ))
 	wageperhour=20
 	if [ $fulldayhour -eq 8 ]
 	then
 		employeesalary=$(( fulldayhour * wageperhour ))
-		echo "Daily Employee wage is:" $employeesalary
+		echo "Daily Employee salary is:" $employeesalary
 	else
-		employeeparttimesalary=$(( fulldayhour * wageperhour ))
-		echo "Part time Employee wage is:" $employeeparttimesalary
+		emplpartsalary=$(( fulldayhour * wageperhour ))
+		echo "Part time Employee salary is:" $emplpartsalary
 	fi
-else
-	echo "Employee is absent"
-fi
+	;;
+0)
+	echo "Employee is absent and salary is 0"
+	;;
+esac
+
