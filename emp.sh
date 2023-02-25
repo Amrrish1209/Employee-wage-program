870d1a73df4805552bba3d71e18f7e0c547cb2b8
Amrrish1209 <roshanbabubl@gmail.com>
2023-02-25 20:15:18 +0530

    [Amrrish] Add : Shell script program to calculate for month employee salary

--- emp.sh
+++ emp.sh
@@ -3,23 +3,31 @@
 echo "Checking Employee is present or absent if present then we want to calculate the Daily wage"
 
 res=$(( RANDOM % 2 ))
+dayhour=$(( RANDOM % 2 == 0 ? 4 : 8 ))
+total_working_days=20
+wageperhour=20
+total_work_hour=0
 
-case $res in 
+for (( day=1;day<=total_working_days;day++ ))
+do
+case $res in
 1)
 	echo "Checking the employee working as fullday or parttime and giving salary"
-	fulldayhour=$(( RANDOM % 2 == 0 ? 4 : 8 ))
-	wageperhour=20
-	if [ $fulldayhour -eq 8 ]
+	if [ $dayhour -eq 8 ]
 	then
-		employeesalary=$(( fulldayhour * wageperhour ))
+		employeesalary=$(( dayhour * wageperhour ))
 		echo "Daily Employee salary is:" $employeesalary
 	else
-		emplpartsalary=$(( fulldayhour * wageperhour ))
+		emplpartsalary=$(( dayhour * wageperhour ))
 		echo "Part time Employee salary is:" $emplpartsalary
 	fi
+	total_work_hour=$(( total_work_hour + dayhour ))
 	;;
 0)
 	echo "Employee is absent and salary is 0"
 	;;
 esac
+done
+	total_salary=$(( total_work_hour * wageperhour ))
+	echo "Total Salary of an Employee is :"$total_salary
 
