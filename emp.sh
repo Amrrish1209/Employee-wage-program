747edf7970268647ad442ccc93ff0d4245b84a32
Amrrish1209 <roshanbabubl@gmail.com>
2023-02-27 13:30:12 +0530

    [Amrrish] Add : Shell script program to refactoring the code and writing the function to get the work hour of the employee

--- emp.sh
+++ emp.sh
@@ -1,37 +1,52 @@
-#!/bin/bash
+#! /bin/bash -x
 
-echo "Checking Employee is present or absent if present then we want to calculate the Daily wage"
-
-
+#constant variable
+is_present_full_time=1
+is_present_part_time=2
+wage_per_hour=20
+max_work_hour=100
 total_working_days=20
-wageperhour=20
+
+#variable
 total_work_hour=0
-max_working_hour=100
 day=1
 
-while (( $day <= $total_working_days && $total_work_hour<=$max_working_hour ))
-do
-res=$(( RANDOM % 2 ))
-dayhour=$(( RANDOM % 2 == 0 ? 4 : 8 ))
-case $res in
-1)
-	echo "Checking the employee working as fullday or parttime and giving salary"
-	if [ $dayhour -eq 8 ]
-	then
-		employeesalary=$(( dayhour * wageperhour ))
-		echo "Full time Employee salary is:" $employeesalary
-	else
-		emplpartsalary=$(( dayhour * wageperhour ))
-		echo "Part time Employee salary is:" $emplpartsalary
-	fi
-	total_work_hour=$(( total_work_hour + dayhour ))
-	;;
-0)
-	echo "Employee is absent and salary is 0"
-	;;
-esac
-((day++))
-done
-	total_salary=$(( total_work_hour * wageperhour ))
-	echo "Total Salary of an Employee is :"$total_salary
+# function to get the work hours of the employee
+function get_work_hours()
+{
+local random_check=$1
+local emp_hours=0
 
+#selection
+case $random_check in
+$is_present_full_time)
+emp_hours=8
+;;
+$is_present_part_time)
+emp_hours=4
+;;
+*)
+emp_hours=0
+;;
+
+esac
+echo $emp_hours
+}
+
+
+#Loop will execute till both the condition is TRUE
+while (($day <= $total_working_days && $total_work_hour <= $max_work_hour ))
+do
+#variable
+random_check=$((RANDOM%3)) # 0/1/2
+
+#calling the function and passing the argument
+emp_hours=$(get_work_hours $random_check)
+
+total_work_hour=$(($total_work_hour + $emp_hours))
+((day++))
+
+done
+
+total_salary=$(( total_work_hour * wage_per_hour))
+echo "Total Salary of an Employee is $total_salary"
