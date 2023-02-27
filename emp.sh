04573e76277655af000b847e6d1ac6bae27f2c93
Amrrish1209 <roshanbabubl@gmail.com>
2023-02-27 13:40:10 +0530

    [Amrrish] Add : Shell script program to storing the daily wage along with the total wage

--- emp.sh
+++ emp.sh
@@ -11,6 +11,9 @@ total_working_days=20
 total_work_hour=0
 day=1
 
+# Declaring an array
+declare -a daily_wages
+
 # function to get the work hours of the employee
 function get_work_hours()
 {
@@ -44,9 +47,14 @@ random_check=$((RANDOM%3)) # 0/1/2
 emp_hours=$(get_work_hours $random_check)
 
 total_work_hour=$(($total_work_hour + $emp_hours))
+
+# Storing the daily wage in the array
+daily_wages[$day]=$(( emp_hours * wage_per_hour))
+
 ((day++))
 
 done
 
 total_salary=$(( total_work_hour * wage_per_hour))
 echo "Total Salary of an Employee is $total_salary"
+echo "Storing the daily wage of an Employee : ${daily_wage[*]}"
